37356d8013320e1a194c06790256d31212e17d35
Xu-xindan <68842260+Xu-xindan@users.noreply.github.com>
2020-11-18 19:03:21 +0800

    Add files via upload

---
+++ test1/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User:XuXindan
+ * DATE:2020/11/18
+ * TIME:13:53
+ */
+//写的Java代码 一般放在groupid.artifactId下
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
